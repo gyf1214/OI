cbddd5459c05c39fc3cfe13a0b1c9d4df1e8b038
gyf1214 <gyf_1214@163.com>
2013-08-17 09:10:46 +0800

    test bash

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo chmod 777 ./$1
+echo $1
+echo
+for ((i = 1; i <= $2; i++))
+do
+	cp $1$i.in $1.in
+	echo $i
+	date +开始%t%S:%N
+	./$1
+	date +结束%t%S:%N
+	echo 
+	diff -b $1.out $1$i.out
+	rm $1.in
+done
