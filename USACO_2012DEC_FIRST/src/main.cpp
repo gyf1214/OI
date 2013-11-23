0170da1bc2f1707b6eb14438e28f61b308b59d65
gyf1214 <gyf_1214@163.com>
2013-11-23 12:25:31 +0800

    add usaco_2012dec_first(need test)

--- USACO_2012DEC_FIRST/src/main.cpp
+++ USACO_2012DEC_FIRST/src/main.cpp
@@ -16,13 +16,139 @@
 #define max(a, b) (a)>(b)?(a):(b)
 #define min(a, b) (a)<(b)?(a):(b)
 #define infi 0x7FFFFFFF
-#define mm 3000000
+#define mm 3001000
+#define em 30010
+#define nm 26
+using namespace std;
 
-struct node
+struct _node
 {
-
+	int p, s, b;
+	int ch;
 	bool end;
-};
+}node[mm];
+int nn;
 
+struct _edge
+{
+	int u, v;
+	int next;
+}edge[nm * nm];
+int head[nm], ne;
+char str[mm];
+bool vis[nm];
 
+int a[em], n;
+bool ans[em];
 
+inline void ins(int u, int v)
+{
+	++ne;
+	edge[ne].next = head[u];
+	edge[ne].u = u;
+	edge[ne].v = v;
+	head[u] = ne;
+}
+
+void dfs(int u)
+{
+	for (int e = head[u]; e; e = edge[e].next)
+	{
+		int v = edge[e].v;
+		if (!vis[v])
+		{
+			vis[v] = true;
+			dfs(v);
+		}
+	}
+}
+
+bool check(int k)
+{
+	clr(head, 0);
+	clr(edge, 0);
+	ne = 0;
+	for (int i = k; i; i = node[i].p)
+	{
+		if (i != k && node[i].end)
+			return false;
+		for (int j = node[node[i].p].s; j; j = node[j].b)
+			if (i != j)
+				ins(node[i].ch, node[j].ch);
+	}
+	rep(i, 0, nm - 1)
+	{
+		clr(vis, 0);
+		dfs(i);
+		if (vis[i])
+			return false;
+	}
+	return true;
+}
+
+void pre()
+{
+	clr(node, 0);
+	nn = 0;
+	scanf("%d", &n);
+	rep(c, 1, n)
+	{
+		scanf("%s", str);
+		int length = strlen(str);
+		int now = 0;
+		rep(i, 0, length - 1)
+		{
+			int ch = str[i] - 'a';
+			int k = 0;
+			for (int j = node[now].s; j; j = node[j].b)
+				if (node[j].ch == ch)
+					k = j;
+			if (k == 0)
+			{
+				++nn;
+				node[nn].p = now;
+				node[nn].b = node[now].s;
+				node[nn].ch = ch;
+				node[now].s = nn;
+				k = nn;
+			}
+			now = k;
+		}
+		node[now].end = true;
+		a[c] = now;
+	}
+}
+
+int main()
+{
+	freopen("first.in", "r", stdin);
+	freopen("first.out", "w", stdout);
+
+	pre();
+	int cnt = 0;
+	clr(ans, 0);
+	rep(i, 1, n)
+		if (check(a[i]))
+		{
+			ans[i] = true;
+			++cnt;
+		}
+	printf("%d\n", cnt);
+	rep(i, 1, n)
+		if (ans[i])
+		{
+			int t = 0;
+			for (int k = a[i]; k; k = node[k].p)
+			{
+				str[t] = node[k].ch + 'a';
+				++t;
+			}
+			for (int k = t - 1; k >= 0; --k)
+				printf("%c", str[k]);
+			printf("\n");
+		}
+
+	fclose(stdin);
+	fclose(stdout);
+	return 0;
+}
