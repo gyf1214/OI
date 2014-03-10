3c24594cb0ad0739bcf631e9241752a21bf6c444
gyf1214 <gyf_1214@163.com>
2014-03-10 17:34:22 +0800

    ac usaco_2006dec_fewcoins

--- USACO_2006DEC_FEWCOINS/src/main.cpp
+++ USACO_2006DEC_FEWCOINS/src/main.cpp
@@ -17,20 +17,81 @@
 #define min(a, b) ((a)<(b)?(a):(b))
 #define infi 0x7FFFFFFF
 #define nm 110
-#define mm 20010
-#define offset 10000
+#define mm 40010
 using namespace std;
 
-int c[nm], v[nm], dp[mm], dp0[mm];
-int n, t;
+int f[mm], g[mm];
+int v[nm], c[nm];
+int a[mm], b[mm];
+int n, m, l, r;
 
-int work()
+void ins(int x, int y)
 {
-	rep(i, -t, t)
-		dp0[i + offset] = infi;
-	dp0[offset] = 0;
-	rep(i, 1, n)
+	while (l <= r && b[r] >= y)
+		--r;
+	++r;
+	a[r] = x;
+	b[r] = y;
 }
 
+int top(int i, int j)
+{
+	while (a[l] < j - c[i])
+		++l;
+	return b[l];
+}
 
+void work1()
+{
+	rep(i, 0, mm - 1)
+		f[i] = infi / 3;
+	f[0] = 0;
+	rep(i, 1, n)
+	{
+		if ((mm - 1) / v[i] < c[i])
+			c[i] = (mm - 1) / v[i];
+		rep(k, 0, v[i] - 1)
+		{
+			l = 1, r = 0;
+			rep(j, 0, (mm - 1 - k) / v[i])
+			{
+				ins(j, f[j * v[i] + k] - j);
+				f[j * v[i] + k] = top(i, j) + j;
+			}
+		}
+	}
+}
 
+void work2()
+{
+	rep(i, 0, mm - 1)
+		g[i] = infi / 3;
+	g[0] = 0;
+	rep(i, 1, n)
+		rep(j, 0, mm - 1)
+			if (j >= v[i])
+				g[j] = min(g[j], g[j - v[i]] + 1);
+}
+
+int main()
+{
+	freopen("fewcoins.in", "r", stdin);
+	freopen("fewcoins.out", "w", stdout);
+
+	scanf("%d%d", &n, &m);
+	rep(i, 1, n)
+		scanf("%d", &v[i]);
+	rep(i, 1, n)
+		scanf("%d", &c[i]);
+
+	work1();
+	work2();
+	int ans = infi / 3;
+	rep(i, 0, mm - m - 1)
+		ans = min(ans, g[m + i] + f[i]);
+	printf("%d\n", ans == infi / 3 ? -1 : ans);
+
+	fclose(stdin);
+	fclose(stdout);
+	return 0;
+}
