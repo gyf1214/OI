7516f934f6b9d6e2b6aa07a894dbcece350c678a
gyf1214 <gyf_1214@163.com>
2014-07-07 17:32:58 +0800

    solve a bug in template(WTF!)

--- template.cpp
+++ template.cpp
@@ -11,7 +11,7 @@
 #define rep(i, a, b) for (int i = (a); i <= (b); ++i)
 #define clr(i, a) memset(i, (a), sizeof(i))
 #define max(a, b) ((a)>(b)?(a):(b))
-#define min(a, b) ((a)>(b)?(a):(b))
+#define min(a, b) ((a)<(b)?(a):(b))
 #define infi 0x7FFFFFFF
 #define mm
 using namespace std;
