1dbeca6ba7b4d9ad26f0735015045ea62ef516ac
gyf1214 <gyf_1214@163.com>
2016-03-05 13:54:54 +0800

    ac cszy_0207

---
+++ CSZY_0207/src/main.cpp
@@ -0,0 +1,24 @@
+//Created At: Sat Mar 5 13:48:40 CST 2016
+//orz zyx
+//ntr czr
+//Author: gyf
+
+#include <iostream>
+using namespace std;
+class Base {
+public:
+    int k;
+    Base(int n):k(n) { }
+};
+class Big  {
+public:
+    int v; Base b;
+
+	Big(int v) : v(v), b(v) {}
+};
+int main()  {
+    Big a1(5);    Big a2 = a1;
+    cout << a1.v << "," << a1.b.k << endl;
+    cout << a2.v << "," << a2.b.k << endl;
+    return 0;
+}
