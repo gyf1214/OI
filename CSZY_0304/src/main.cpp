658951e280d86e45a62ebbd04cbc4de122000db9
gyf1214 <gyf1214@gmail.com>
2016-03-10 13:39:06 +0800

    ac cszy_0304

---
+++ CSZY_0304/src/main.cpp
@@ -0,0 +1,38 @@
+//Created At: Thu Mar 10 13:34:00 CST 2016
+//orz zyx
+//ntr czr
+//Author: gyf
+
+#include <iostream>
+using namespace std;
+class A {
+	private:
+	int nVal;
+	public:
+	void Fun()
+	{ cout << "A::Fun" << endl; };
+	void Do()
+	{ cout << "A::Do" << endl; }
+};
+class B:public A {
+	public:
+	virtual void Do()
+	{ cout << "B::Do" << endl;}
+};
+class C:public B {
+	public:
+	void Do( )
+	{ cout <<"C::Do"<<endl; }
+	void Fun()
+	{ cout << "C::Fun" << endl; }
+};
+void Call(
+	B &p
+) {
+	p.Fun(); p.Do();
+}
+int main() {
+	C c;
+	Call( c);
+	return 0;
+}
